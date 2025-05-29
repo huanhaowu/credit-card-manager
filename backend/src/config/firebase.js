d22565bcc13767fc38b8bcad53c64d99addf19f9
huanhaowu <1104326@est.intec.edu.do>
2025-05-28 22:13:08 -0400

    feat: add firebase connection

---
+++ backend/src/config/firebase.js
@@ -0,0 +1,11 @@
+const admin = require("firebase-admin");
+const path = require("path");
+
+const serviceAccount = require(path.resolve(__dirname, "../../firebase-service-account.json"));
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
+module.exports = db;
